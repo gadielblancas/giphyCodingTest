da49d46ee98c4b34e755e2611466588c6e360a69
gadielblancas <gadiel.blancas@gmail.com>
2018-11-05 23:01:25 -0800

    test

--- src/giphyList.js
+++ src/giphyList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-export default class GiphyList extends React.Component{
+export default class GiphyList extends React.Component {
 
    renderListElmnt(giphyElmnt){
 
