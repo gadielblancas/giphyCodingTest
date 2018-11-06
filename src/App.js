99877d38fd235265822080a0dcbb8c0e11fb05b1
gadielblancas <gadiel.blancas@gmail.com>
2018-11-05 22:51:19 -0800

    first commit :)

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import GiphyList from './giphyList'
+import './App.css';
+
+class App extends Component {
+
+    //api key
+    apiKey = 'CxRm9K3L2LmjbhLSFWFZa18pSvL6ZeMU';
+    //building end point
+    url = `http://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&limit=6&q=ryan+gosling`;
+
+    state = {
+      data:[]
+    };
+
+    getGifs(event){
+      //geting the event value
+      let searchStr = event.target.value;
+      //adding + to whitespaces
+      searchStr = searchStr.replace(/\s/g, "+");
+      //final url string
+      let endpoint = this.url + '&q=' + searchStr;
+
+      //service call
+      fetch(endpoint)
+      .then(response => response.json())
+      .then(data =>{
+
+          this.setState({
+            data:data.data
+          });
+
+
+      });
+
+    }
+    render() {
+        console.log(this.state.data);
+
+        return (
+          <div className="container-fluid">
+             <div className="search-conteiner">
+                  <input className="form-control"
+                  onChange={ (event) => this.getGifs(event) }
+                  placeholder="search for gifs"
+                  type="text"></input>
+             </div>
+
+              <div className="container-fluid">
+                <GiphyList
+                 giphydata={this.state.data}/>
+              </div>
+          </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/giphyList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+export default class GiphyList extends React.Component{
+
+   renderListElmnt(giphyElmnt){
+
+     return(
+        <div key={giphyElmnt.id} className="embed-responsive embed-responsive-16by9 col-sm-4 col-md-4 col-xl-4 animated fadeIn fast">
+         <iframe src={giphyElmnt.embed_url} width="480" height="270" frameBorder="0" className="giphy-embed embed-responsive-item gif-cell" allowFullScreen></iframe>
+        </div>
+     );
+
+   }
+
+   render(){
+
+       let display;
+       let title;
+
+       if(this.props.giphydata.length ){
+         title = <div className="results-container">Results:</div>;
+         display = this.props.giphydata.map( this.renderListElmnt );
+       }else{
+         title = '';
+         display = '';
+       }
+
+       return(
+        <div className="row">
+          {title}
+          {display}
+        </div>
+
+       );
+   }
+
+
+}
