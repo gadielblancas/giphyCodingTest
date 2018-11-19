baabb6159788432d16b88ef75c0730867287d6ea
gadielblancas <gadiel.blancas@gmail.com>
2018-11-18 16:34:09 -0800

    jsx refactor

--- src/giphElement.js
+++ src/giphElement.js
@@ -30,15 +30,19 @@ export default class GifElement extends React.Component{
      if(!this.state.isHover){
 
        output = (
-
-           <img src={ this.props.dataElmnt['images']['480w_still']['url'] } className="img-fluid mx-auto d-block image-thumb gif-cell"/>
-
+         <img src={ this.props.dataElmnt['images']['480w_still']['url'] }
+              className="img-fluid mx-auto d-block image-thumb gif-cell"/>
        );
 
     }else{
        output = (
          <div className="embed-responsive embed-responsive-16by9 animated fadeIn fast gif-cell">
-             <iframe src={this.props.dataElmnt.embed_url} width="480" height="270" frameBorder="0" className="giphy-embed embed-responsive-item" allowFullScreen></iframe>
+             <iframe src={this.props.dataElmnt.embed_url}
+                     width="480"
+                     height="270"
+                     frameBorder="0"
+                     className="giphy-embed embed-responsive-item"
+                     allowFullScreen></iframe>
          </div>
        );
     }
